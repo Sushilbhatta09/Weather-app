4e58cad4e8a4ba503ba09631206fed64076dd845
Sushilbhatta09 <87477889+Sushilbhatta09@users.noreply.github.com>
2021-07-17 18:29:20 +0545

    Update weather.py

--- weather.py
+++ weather.py
@@ -13,50 +13,50 @@ def write(data):
         json.dump(data,file)
 
 def getweather(window):
-    city = textfield.get()
-    api_response_url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APPID
-    response = requests.get(api_response_url)
-    response_data = response.json()
-    write(response_data)
+        city = textfield.get()
+        api_response_url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APPID
+        response = requests.get(api_response_url)
+        response_data = response.json()
+        write(response_data)
 
 
 
-    #take data from response
-try :
-    condition = response_data['weather'][0]['main']
-
-    
-     #changing background image, reading the image
-    bg_image_data=Image.open('./Images/'+condition+'.jpg')
-    resized_image_data = bg_image_data.resize((800,800),Image.ANTIALIAS)
-    bg_image=ImageTk.PhotoImage(resized_image_data)
+        #take data from response
+    try :
+        condition = response_data['weather'][0]['main']
 
 
-    temp = int(response_data['main']['temp']-273)
-    temp_min = int(response_data['main']['temp_min']-273)
-    temp_max = int(response_data['main']['temp_max']-273)
-    pressure = response_data['main']['pressure']
-    humidity = response_data['main']['humidity']
-    wind = response_data['wind']['speed']
-    sunrise = response_data['sys']['sunrise']
-    sunrise1 = datetime.datetime.fromtimestamp(sunrise)
-    sunset = response_data['sys']['sunset']
-    sunset1 = datetime.datetime.fromtimestamp(sunset)
+         #changing background image, reading the image
+        bg_image_data=Image.open('./Images/'+condition+'.jpg')
+        resized_image_data = bg_image_data.resize((800,800),Image.ANTIALIAS)
+        bg_image=ImageTk.PhotoImage(resized_image_data)
 
-    #merge all information in single variable
 
-    final_data = f"{condition} \n {temp} °C"
-    final_info = f"Min_temp:{temp_min}°C \n Max_Temp:{temp_max}°C \n Pressure:{pressure} \n Humidity:{humidity} \n Windspeed:{wind} \n Sunrise:{sunrise1} \n Sunset:{sunset1}"
-    label1.config(text=final_data)
-    label2.config(text=final_info)
-    label3.configure(image=bg_image) 
-    #configure for image
-    label3.image=(bg_image)
-    label3.image=(bg_image)
-   #  icons_label.configure(image=icon_image)
-   # icons_label.image=(icon_image)
-    #country_label.configure(image=country_image)
-   # country_label.image=(country_image)
+        temp = int(response_data['main']['temp']-273)
+        temp_min = int(response_data['main']['temp_min']-273)
+        temp_max = int(response_data['main']['temp_max']-273)
+        pressure = response_data['main']['pressure']
+        humidity = response_data['main']['humidity']
+        wind = response_data['wind']['speed']
+        sunrise = response_data['sys']['sunrise']
+        sunrise1 = datetime.datetime.fromtimestamp(sunrise)
+        sunset = response_data['sys']['sunset']
+        sunset1 = datetime.datetime.fromtimestamp(sunset)
+
+        #merge all information in single variable
+
+        final_data = f"{condition} \n {temp} °C"
+        final_info = f"Min_temp:{temp_min}°C \n Max_Temp:{temp_max}°C \n Pressure:{pressure} \n Humidity:{humidity} \n Windspeed:{wind} \n Sunrise:{sunrise1} \n Sunset:{sunset1}"
+        label1.config(text=final_data)
+        label2.config(text=final_info)
+        label3.configure(image=bg_image) 
+        #configure for image
+        label3.image=(bg_image)
+        label3.image=(bg_image)
+       #  icons_label.configure(image=icon_image)
+       # icons_label.image=(icon_image)
+        #country_label.configure(image=country_image)
+       # country_label.image=(country_image)
 except KeyError:
     label1.config(text=response_data['message'])
     label2.config(text=response_data['cod'])
